627796dc8c46fba46e3436916493c457deed488e
Shawn Garrison <sg1990@comcast.net>
2019-03-02 17:36:44 -0500

    fixed a bug that needs to handle issues when the word can not be compressed and handles ones better

--- chapter 1/6.py
+++ chapter 1/6.py
@@ -2,21 +2,27 @@ def stringcompress(word):
     newword = ""
     pos = 0
     counter = 1
-    print(len(word))
+    check = False
     while pos < len(word):
         char = word[pos]
         while pos < len(word)-1 and  word[pos] == word[pos+1]:
             counter = counter + 1
             pos = pos + 1   
-        if counter > 1:
+        if counter > 0:
             newword = newword + char+ str(counter)
+            if counter > 1:
+                check = True
             counter = 1
-        else:
-            newword = newword + char 
-        pos = pos + 1 
+        #else:
+            #newword = newword + char 
+        pos = pos + 1
+    if check:
+        return newword
+    return word
+    
+    
             
             
    
-    return newword
-print(stringcompress('AAAAbbbAAA2232Ssss'))
+print(stringcompress('aabBBBBcccc'))
 
