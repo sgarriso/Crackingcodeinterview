d7fd8508b98b5f2d2e63eb5eb92c2a5dbdcc4615
Shawn Garrison <sg1990@comcast.net>
2019-03-02 23:14:38 -0500

    solved without buffer dups

--- chapter 2/1.py
+++ chapter 2/1.py
@@ -4,12 +4,38 @@ def buildtest():
     
     list = SLinkedList()
     list.Atbegining("Sun")
+    list.Atbegining("Sun")
+    list.Atbegining("Sun")
     list.Atbegining("Mon")
+    list.Atbegining("Sun")
     list.Atbegining("Mon")
+    list.Atbegining("2")
+    list.Atbegining("2")
+    list.Atbegining("5")
+    list.Atbegining("2")
     #list.LListprint()
     return list
-def removedups_withoutbuffer(head):
-    return head
+def removedups_withoutbuffer(List):
+    if not List.head:
+        return List
+    node = List.head
+    data = node.data
+    while node:
+        perv = node
+        check = node.next
+        while check:
+            if check.data == data:
+                # remove check
+                perv.next = check.next
+            else:
+                perv = perv.next
+            check = check.next
+        node = node.next
+        if node:
+            data = node.data
+            
+        
+    return List
 def removedups(List):
     new_list = SLinkedList()
     check_list = []
@@ -23,7 +49,7 @@ def removedups(List):
 list = buildtest()
 list.LListprint()
 print("remove dups")
-list = removedups(list)
+list = removedups_withoutbuffer(list)
 list.LListprint()
 
 
